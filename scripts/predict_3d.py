ebc413ca0099013c2a63125f00f809a4df04f9ee
Jennifer Hill <jennifer.hill@berkeley.edu>
2025-11-10 15:06:51 -0500

    Add script to predict from 3d crops

--- scripts/predict_3d.py
+++ scripts/predict_3d.py
@@ -1,77 +1,184 @@
-"""Sample script to detect spots on a 3D model with Spotiflow.
-"""
+import json
+import dask.array as da
+import numpy as np
+import napari
+import torch
+import os
+import pandas as pd
 
-import argparse
-import logging
-import sys
-from pathlib import Path
-
-import tifffile
-from skimage import io
-from spotiflow import utils
 from spotiflow.model import Spotiflow
-from spotiflow.model.pretrained import list_registered
+from spotiflow.augmentations.transforms3d import Crop3D
+from spotiflow.data.spots2dt import Spots2DT
+from spotiflow.utils.matching import points_matching
 
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+def load_zarr(zarr_path: str):
+    try:
+        dataset = da.from_zarr(zarr_path)
+        if dataset.dtype.byteorder == '>':
+            dataset = dataset.astype(dataset.dtype.newbyteorder('<'))
+        print(f"Loaded dataset from {zarr_path} with shape {dataset.shape}")
+        return dataset
+    except Exception as e:
+        print(f"Error loading dataset from {zarr_path}: {e}")
+        return None
+    
+def crop_manual(image: da.Array, annotation: np.ndarray, crop_size: tuple):
+    cz, cy, cx = crop_size
+    assert len(crop_size) == 3, "crop_size must be a 3-tuple (z, y, x)"
+    img_z, img_y, img_x = image.shape[:3]
+    # ensure crop fits within the image dimensions
+    assert cz <= img_z and cy <= img_y and cx <= img_x, "crop_size must be <= image.shape in all dimensions"
 
-IMAGE_EXTENSIONS = ("tif", "tiff", "png", "jpg", "jpeg")
+    z, y, x = map(int, annotation)
 
+    # center the crop on the annotation then clamp to image bounds so crop size is exact
+    z_start = int(z - cz // 2)
+    y_start = int(y - cy // 2)
+    x_start = int(x - cx // 2)
+
+    z_start = max(0, min(z_start, img_z - cz))
+    y_start = max(0, min(y_start, img_y - cy))
+    x_start = max(0, min(x_start, img_x - cx))
+
+    z_end = z_start + cz
+    y_end = y_start + cy
+    x_end = x_start + cx
+
+    cropped_image = image[z_start:z_end, y_start:y_end, x_start:x_end]
+    cropped_annotation = np.array([z - z_start, y - y_start, x - x_start])
+
+    return cropped_image, cropped_annotation
+
+def extract_matched_pairs(stats, annotation, spots):
+    '''Extract matched pairs of annotations and predicted spots based on matching statistics.
+    Args:
+        stats: Matching statistics object containing indices of matched pairs.
+        annotations: Array of annotation coordinates (t, z, y, x).
+        spots: Array of predicted spot coordinates (z, y, x).
+    Returns:
+        Array of matched pairs with shape (N, 2, 4) where N is the number of matched pairs,
+        and each pair contains the annotation and corresponding predicted spot coordinates 
+        (t, x, y, z).
+    '''
+    pairs_ids = np.array(stats.matched_pairs)
+    print(f"pairs_ids: {pairs_ids}")
+    print(f"annotation: {annotation}")
+    pairs = np.zeros((pairs_ids.shape[0], 2, 4), dtype=np.float32)
+    pairs[:, 0, :] = annotation[0][:, [0, 3, 2, 1]] # reorder to t,x,y,z
+    pairs[:, 1, 1:] = spots[pairs_ids[:, 1]][:, [2, 1, 0]] # add x,y,z
+    pairs[:, 1, 0] = pairs[:, 0, 0] # add t from annotations
+    return pairs
+
+def main(data_dir: str, model_time: str, early_predict_frames: int = 1, prob_thresh: float = 0.5):
+
+    print(f"Predicting with model {model_time}")
+
+    model_path = "/groups/sgro/sgrolab/jennifer/spotiflow_mbl/scripts/outputs/spotiflow-" + model_time
+    model = Spotiflow.from_folder(model_path, map_location="cuda")
+    model.to(torch.device("cuda"))
+
+    with open(data_dir, "r") as f:
+        data = json.load(f)
+    val_data_list = list(data.items())[-3:]  # last 3 are validation datasets
+
+    # Initialize metrics dataframe
+    metrics = pd.DataFrame(columns=["dataset", "TP", "FP", "FN"])
+    dist_error_list = []
+    z_dist_error_list = []
+    matched_pairs_data = []
+
+    for i, zarr_dataset in enumerate(val_data_list):
+        print(f"Processing dataset {i}...")
+        zarr_dataset_path = zarr_dataset[0]
+
+        # Process annotations
+        ds_annotations = zarr_dataset[1]
+        # ds_annotations = np.array(ds_annotations) # tzyx
+
+        dataset = load_zarr(os.path.join(zarr_dataset_path, "0", "0"))
+
+        tps = 0
+        fps = 0
+        fns = 0
+
+        for full_annot in ds_annotations:
+            tp = full_annot[0] - early_predict_frames
+            img = dataset[tp, 0]  # cells are channel 0
+            annot = np.array(full_annot)[-3:]  # z,y,x
+
+            # Crop image and annotation manually
+            cropped_img, cropped_annot = crop_manual(img, annot, crop_size=(64, 256, 256))
+
+            # Predict
+            spots, details = model.predict(
+                img=cropped_img.astype(np.float32).compute(),
+                subpix=True,
+                min_distance=75,
+                prob_thresh=prob_thresh,
+                #n_tiles=n_tiles, # change if you run out of memory
+                device="cuda",
+            )
+
+            print(f"p1: {np.array(spots)}, p2: {np.array([cropped_annot])}")
+            # Evaluate
+            stats = points_matching(
+                        p1=np.array([cropped_annot]),
+                        p2=np.array(spots),
+                        cutoff_distance=20,
+                        eps=1e-8,
+                    )
+            print(f"TP: {stats.tp}, FP: {stats.fp}, FN: {stats.fn}")
+            tps += stats.tp
+            fps += stats.fp
+            fns += stats.fn
+
+            viewer = napari.Viewer()
+            # viewer.add_image(imgs[-1])
+            #viewer.add_points(all_annotations[pred_dataset], size=20, name="pts", symbol="disc", border_color="red", face_color="red")
+            viewer.add_image(cropped_img, name="img")
+            viewer.add_points(spots, size=20, name="pts", symbol="disc", border_color="blue", face_color="blue")
+            viewer.add_points(cropped_annot, size=20, name="annotations", symbol="disc", border_color="red", face_color="red")
+            viewer.add_image(details.heatmap, name="pred_heatmap", colormap="magma", blending="additive", opacity=0.6)
+            viewer.add_image((details.flow+1)*0.5, name="flow")
+            napari.run()
+
+            if len(stats.matched_pairs) > 0:
+                assert len(stats.matched_pairs) == 1, "Expected exactly one matched pair"
+                spot = spots[stats.matched_pairs[0][0]]
+                dist_error_list.extend(stats.dist.tolist()) # calculated as error in t, x, y where t has been shifted forward
+                z_dist_error = abs(cropped_annot[0] - spot[0])
+                z_dist_error_list.append(z_dist_error)
+                cropped_annot = [int(tp), int(cropped_annot[0]), int(cropped_annot[1]), int(cropped_annot[2])]  # add time coordinate 
+                matched_pairs_data.append({
+                    "dataset": zarr_dataset_path,
+                    "matched_pair": [full_annot, cropped_annot, spot.tolist()],
+                    "distance_error": stats.dist.tolist(),
+                    "z_distance_error": float(z_dist_error),
+                })
+        
+        print(f"Dataset {i} - TP: {tps}, FP: {fps}, FN: {fns}")
+        metrics = pd.concat([metrics, pd.DataFrame({"dataset": [zarr_dataset_path], "TP": [tps], "FP": [fps], "FN": [fns]})], ignore_index=True)
+
+    # Save metrics to CSV
+    avg_dist_error = np.mean(dist_error_list) if len(dist_error_list) > 0 else 0
+    print(f"Average distance error across all datasets: {avg_dist_error}")
+    avg_z_dist_error = np.mean(z_dist_error_list) if len(z_dist_error_list) > 0 else 0
+    print(f"Average z distance error across all datasets: {avg_z_dist_error}")
+    # Add average distance error to metrics dataframe
+    metrics["avg_distance_error"] = avg_dist_error
+    metrics["avg_z_distance_error"] = avg_z_dist_error
+    metrics_path = os.path.join(model_path, f"prediction_metrics_{prob_thresh}pt.csv")
+    metrics.to_csv(metrics_path, index=False)
+
+    # Save matched pairs data to JSON
+    matched_pairs_path = os.path.join(model_path, f"matched_pairs_{prob_thresh}pt.json")
+    with open(matched_pairs_path, "w") as f:
+        json.dump(matched_pairs_data, f, indent=4)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=Path)
-    parser.add_argument("--output", type=Path)
-    parser.add_argument("--model", type=str, default="smfish_3d")
-    parser.add_argument("--channel", type=int, required=False, default=None)
-    parser.add_argument("--debug", action="store_true", default=False)
-    parser.add_argument("--min-distance", type=int, default=1, required=True)
-    parser.add_argument("--max-yx-tile-size", type=int, default=256)
-    parser.add_argument("--max-z-tile-size", type=int, default=32)
-    args = parser.parse_args()
-
-    if args.model not in list_registered():
-        args.model = Path(args.model)
-        assert args.model.exists(), f"Model not found: {args.model}"
-        
-    assert args.input.exists(), f"Input file not found: {args.input}"
-    assert args.output.suffix == ".csv", f"Output file must have a .csv extension"
-
-
-    print("Reading input data...")
-    img = io.imread(str(args.input))
-    if img.ndim == 4:
-        assert args.channel is not None, "Channel argument required if input is 4D (C,Z,Y,X)"
-        img = img[args.channel]
-    elif args.channel is not None:
-        print("Ignoring channel argument as input is (Z,Y,X)")
-
-    if args.debug:
-        print("Debug mode. Will predict on crop")
-        img = img[128:192, 1000:1256, 1000:1256]
-    print("Loading model...")
-    if args.model not in list_registered():
-        model = Spotiflow.from_folder(args.model, map_location="auto")
-    else:
-        model = Spotiflow.from_pretrained(args.model, map_location="auto")
-        
-
-    print(f"Image shape is: {img.shape}")
-
-    args.output.parent.mkdir(parents=True, exist_ok=True)
-
-    n_tiles = tuple(max(s//g, 1) for s, g in zip(img.shape, (args.max_z_tile_size, args.max_yx_tile_size, args.max_yx_tile_size)))
-    print(n_tiles)
-    print("Predicting volume...")
-    spots, details = model.predict(
-        img,
-        subpix=True,
-        n_tiles=n_tiles, # change if you run out of memory
-        device="auto",
-        min_distance=args.min_distance,
-    )
-
-    if not args.debug:
-        utils.write_coords_csv(spots, args.output)
-
-    if args.debug:
-        tifffile.imwrite(args.output.parent/"img_debug.tif", img)
+    data_dir = "/groups/sgro/sgrolab/jennifer/predicty/training_data_server.json"
+    main(data_dir, 
+         model_time = "20251107_1721",
+         early_predict_frames=1,
+         prob_thresh = 0.2,
+         )
